4554784be5467c9a741c78fd3e5833cd08a677f6
v0x0g <v0x0g_dev@proton.me>
2023-12-14 15:08:46 +1000

    Moving part 2: Electric Boogaloo

--- rayna_ui_base/src/lib.rs
+++
@@ -1,6 +0,0 @@
-//! A simple "interface" crate that separates the crate [rayna_ui]
-//! From the actual implementation of the egui backend, providing
-//! a slight abstraction and separation for it
-
-pub mod app;
-pub mod backend;

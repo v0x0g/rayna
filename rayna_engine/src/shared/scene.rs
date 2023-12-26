2305694a8453bea7bc0400305b2f0641ef4f3075
v0x0g <v0x0g_dev@proton.me>
2023-12-26 20:53:46 +1000

    Reformat scene.rs because rustfmt sucks ass with macro calls

--- rayna_engine/src/shared/scene.rs
+++ rayna_engine/src/shared/scene.rs
@@ -9,6 +9,7 @@ use crate::skybox::SkyboxType;
 use rayna_shared::def::types::{Angle, Pixel, Point3, Vector3};
 
 #[macro_export]
+#[rustfmt::skip] // rustfmt is shit with macros
 macro_rules! scene {
     {
         camera: $cam:expr,
@@ -35,46 +36,47 @@ pub struct Scene {
 
 impl Scene {
     pub fn simple() -> Self {
+        #[rustfmt::skip]
         scene! {
-                    camera: Camera {
-                        pos: Point3::new(0., 0.5, -3.),
-                        fwd: Vector3::Z,
-                        v_fov: Angle::from_degrees(45.),
-                    focus_dist: 3.,
-                    defocus_angle: Angle::from_degrees(10.)
-                    },
-                    objects: [
-                        Sphere { // Small, top
-                            pos: Point3::new(0., 0., 1.),
-                            radius: 0.5,
-                            material: (MetalMaterial {
-                                albedo: Pixel::from([0.8; 3]),
-                                fuzz: 1.
-                            })
-        .into()                },
-                        Sphere { // Ground
-                            pos: Point3::new(0., -100.5, -1.),
-                            radius: 100.,
-                            material: (LambertianMaterial {
-                                albedo: Pixel::from([0.5;3]),
-                            })
-        .into()                }
-                    ]
+            camera: Camera {
+                pos: Point3::new(0., 0.5, -3.),
+                fwd: Vector3::Z,
+                v_fov: Angle::from_degrees(45.),
+                focus_dist: 3.,
+                defocus_angle: Angle::from_degrees(10.)
+            },
+            objects: [
+                Sphere { // Small, top
+                    pos: Point3::new(0., 0., 1.),
+                    radius: 0.5,
+                    material: MetalMaterial {
+                        albedo: Pixel::from([0.8; 3]),
+                        fuzz: 1.
+                    }.into()
+                },
+                Sphere { // Ground
+                    pos: Point3::new(0., -100.5, -1.),
+                    radius: 100.,
+                    material: LambertianMaterial {
+                        albedo: Pixel::from([0.5;3]),
+                    }.into()
                 }
+            ]
+        }
     }
 
     pub fn trio() -> Self {
-        let material: MaterialType = (LambertianMaterial {
+        let material: MaterialType = LambertianMaterial {
             albedo: Pixel::from([1.; 3]),
-        })
+        }
         .into();
         scene! {
             camera: Camera {
                 pos: Point3::new(0., 0., -3.),
                 fwd: Vector3::Z,
                 v_fov: Angle::from_degrees(45.),
-            focus_dist: 3.,
-            defocus_angle: Angle::from_degrees(10.)
+                focus_dist: 3.,
+                defocus_angle: Angle::from_degrees(3.)
             },
             objects: [
                 Sphere { // Left, big
@@ -107,60 +109,60 @@ impl Scene {
             fwd: Vector3::new(0., 0., -1.).normalize(),
             v_fov: Angle::from_degrees(45.),
             focus_dist: 3.,
-            defocus_angle: Angle::from_degrees(10.),
+            defocus_angle: Angle::from_degrees(3.),
         };
 
         let mut objects = Vec::<ObjectType>::new();
 
         // Ground
         objects.push(
-            (Sphere {
+            Sphere {
                 pos: Point3::new(0., -100.5, -1.),
                 radius: 100.,
-                material: (LambertianMaterial {
+                material: LambertianMaterial {
                     albedo: [0.8, 0.8, 0.0].into(),
-                })
+                }
                 .into(),
-            })
+            }
             .into(),
         );
 
         // Left
         objects.push(
-            (Sphere {
+            Sphere {
                 pos: Point3::new(-1., 0., -1.),
                 radius: 0.5,
-                material: (DielectricMaterial {
+                material: DielectricMaterial {
                     albedo: [1.; 3].into(),
                     refractive_index: 1.5,
-                })
+                }
                 .into(),
-            })
+            }
             .into(),
         );
         // Mid
         objects.push(
-            (Sphere {
+            Sphere {
                 pos: Point3::new(0., 0., -1.),
                 radius: 0.5,
-                material: (LambertianMaterial {
+                material: LambertianMaterial {
                     albedo: [0.1, 0.2, 0.5].into(),
-                })
+                }
                 .into(),
-            })
+            }
             .into(),
         );
         // Right
         objects.push(
-            (Sphere {
+            Sphere {
                 pos: Point3::new(1., 0., -1.),
                 radius: 0.5,
-                material: (MetalMaterial {
+                material: MetalMaterial {
                     albedo: [0.8, 0.6, 0.2].into(),
                     fuzz: 0.,
-                })
+                }
                 .into(),
-            })
+            }
             .into(),
         );
 
@@ -183,38 +185,38 @@ impl Scene {
         let mut objects = Vec::<ObjectType>::new();
 
         objects.push(
-            (Sphere {
+            Sphere {
                 pos: Point3::new(0., 1., 0.),
                 radius: 1.,
-                material: (DielectricMaterial {
+                material: DielectricMaterial {
                     refractive_index: 1.5,
                     albedo: [1.; 3].into(),
-                })
+                }
                 .into(),
-            })
+            }
             .into(),
         );
         objects.push(
-            (Sphere {
+            Sphere {
                 pos: Point3::new(-4., 1., 0.),
                 radius: 1.,
-                material: (LambertianMaterial {
+                material: LambertianMaterial {
                     albedo: [0.7, 0.6, 0.5].into(),
-                })
+                }
                 .into(),
-            })
+            }
             .into(),
         );
         objects.push(
-            (Sphere {
+            Sphere {
                 pos: Point3::new(4., 1., 0.),
                 radius: 1.,
-                material: (MetalMaterial {
+                material: MetalMaterial {
                     albedo: [0.7, 0.6, 0.5].into(),
                     fuzz: 0.,
-                })
+                }
                 .into(),
-            })
+            }
             .into(),
         );
 

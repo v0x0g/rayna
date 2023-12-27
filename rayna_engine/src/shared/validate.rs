5cd2bda9b0910517f2b01bef041b7c07089b3014
v0x0g <v0x0g_dev@proton.me>
2023-12-27 18:33:58 +1000

    Better validation messages

--- rayna_engine/src/shared/validate.rs
+++ rayna_engine/src/shared/validate.rs
@@ -17,6 +17,63 @@ pub const EPSILON: Number = 1e-6;
 pub const ULPS: usize = 4;
 pub const RELATIVE: Number = 1e-3;
 
+#[inline(always)]
+#[track_caller]
+pub fn number(val: impl Borrow<Number>) {
+    debug_assert_only!();
+
+    let val = val.borrow();
+    assert!(!val.is_nan(), "should not be nan; val: {val}");
+}
+
+#[inline(always)]
+#[track_caller]
+pub fn normal3(n: impl Borrow<Vector3>) {
+    debug_assert_only!();
+    let n = n.borrow();
+    vector3(n);
+    assert!(
+        n.is_normalized(),
+        "should be normalised; vec: {n:?}, len: {:?}",
+        n.length()
+    );
+}
+
+#[inline(always)]
+#[track_caller]
+pub fn point3(v: impl Borrow<Point3>) {
+    debug_assert_only!();
+    let v = v.borrow();
+    assert!(!v.is_nan(), "should not be nan; vec: {v:?}");
+}
+
+#[inline(always)]
+#[track_caller]
+pub fn vector3(v: impl Borrow<Vector3>) {
+    debug_assert_only!();
+    let v = v.borrow();
+    assert!(!v.is_nan(), "should not be nan; vec: {v:?}");
+}
+
+#[inline(always)]
+#[track_caller]
+pub fn ray(r: impl Borrow<Ray>) {
+    debug_assert_only!();
+    let r = r.borrow();
+    normal3(r.dir());
+}
+
+#[inline(always)]
+#[track_caller]
+pub fn colour(c: impl Borrow<Pixel>) {
+    debug_assert_only!();
+    let c = c.borrow();
+    assert!(
+        c.0.iter().all(|&chan| chan >= 0.0),
+        "channels should be >= 0; col: {c:?}"
+    )
+}
+
 /// Asserts that an intersection was valid
 #[inline(always)]
 #[track_caller]
@@ -50,62 +107,20 @@ pub fn intersection(
         max_relative = RELATIVE
     );
 
-    // TODO: Ray::at(dist) == intersect.pos
+    assert!(
+        Point3::relative_eq(
+            &intersect.pos,
+            &ray.at(intersect.dist),
+            EPSILON,
+            RELATIVE
+        ),
+        "intersect position doesn't match ray at intersection dist; intersect_pos: {i_pos:?}, dist: {dist}, ray: {ray:?}, ray_pos: {r_pos:?}",
+        i_pos = intersect.pos,
+        dist = intersect.dist,
+        ray = ray,
+        r_pos = ray.at(intersect.dist)
+    );
 
     normal3(intersect.ray_normal);
     normal3(intersect.normal);
-}
-
-#[inline(always)]
-#[track_caller]
-pub fn number(x: impl Borrow<Number>) {
-    debug_assert_only!();
-
-    let x = x.borrow();
-    assert!(!x.is_nan(), " [nan check] {x}");
-}
-
-#[inline(always)]
-#[track_caller]
-pub fn normal3(n: impl Borrow<Vector3>) {
-    debug_assert_only!();
-    let n = n.borrow();
-    vector3(n);
-    assert!(
-        n.is_normalized(),
-        "[normalised] {n:?} (len: {:?})",
-        n.length()
-    );
-}
-
-#[inline(always)]
-#[track_caller]
-pub fn point3(v: impl Borrow<Point3>) {
-    debug_assert_only!();
-    let v = v.borrow();
-    assert!(!v.is_nan(), "[nan] {v:?}");
-}
-
-#[inline(always)]
-#[track_caller]
-pub fn vector3(v: impl Borrow<Vector3>) {
-    debug_assert_only!();
-    let v = v.borrow();
-    assert!(!v.is_nan(), "[nan] {v:?}");
-}
-
-#[inline(always)]
-#[track_caller]
-pub fn ray(r: impl Borrow<Ray>) {
-    debug_assert_only!();
-    let r = r.borrow();
-    normal3(r.dir());
-}
-
-#[inline(always)]
-#[track_caller]
-pub fn colour(c: impl Borrow<Pixel>) {
-    debug_assert_only!();
-    let c = c.borrow();
-    assert!(c.0.iter().all(|&chan| chan >= 0.0), "[channels >= 0] {c:?}")
 }

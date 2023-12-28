ad6ab5956697d1701fed31ce4f5ed512669909ca
v0x0g <v0x0g_dev@proton.me>
2023-12-28 16:56:47 +1000

    Add some util methods for aabb

--- rayna_engine/src/accel/aabb.rs
+++ rayna_engine/src/accel/aabb.rs
@@ -7,7 +7,7 @@ use rayna_shared::def::types::{Number, Point3};
 /// An **Axis-Aligned Bounding Box** (AABB)
 ///
 /// The box spans between the two corners `min` and `max`'
-#[derive(CopyGetters, Copy, Clone, Debug, PartialEq)]
+#[derive(CopyGetters, Copy, Clone, Debug, PartialEq, Default)]
 #[getset(get_copy)]
 pub struct Aabb {
     /// The lower corner of the [Aabb]; the corner with the smallest coordinates
@@ -21,6 +21,30 @@ impl Aabb {
     pub fn new(a: Point3, b: Point3) -> Self {
         let min = Point3::min(a, b);
         let max = Point3::max(a, b);
+        Self { min, max }
+    }
+
+    /// Returns an [Aabb] that surrounds the two given boxes
+    pub fn encompass(a: Self, b: Self) -> Self {
+        let min = Point3::min(a.min, b.min);
+        let max = Point3::max(a.max, b.max);
+        Self { min, max }
+    }
+
+    /// [Self::encompass] but for an arbitrary number of boxes
+    pub fn encompass_iter(iter: impl IntoIterator<Item = Aabb>) -> Self {
+        iter.into_iter().reduce(Self::encompass).unwrap_or_default()
+    }
+
+    //noinspection RsBorrowChecker - it's just plain wrong, doesn't recognise `p: Point3` and is `Copy`
+    /// [Self::encompass] but for an arbitrary number of points
+    pub fn encompass_points(iter: impl IntoIterator<Item = Point3>) -> Self {
+        let mut min = Point3::ZERO;
+        let mut max = Point3::ZERO;
+        for p in iter.into_iter() {
+            min = min.min(p);
+            max = max.max(p);
+        }
         Self { min, max }
     }
 

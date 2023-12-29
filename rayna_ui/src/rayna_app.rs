f8b5fcc97784001adafeeb29d8c3883075c147dc
v0x0g <v0x0g_dev@proton.me>
2023-12-29 23:36:11 +1000

    This BVH is REALLY slow for some reason...

--- rayna_ui/src/rayna_app.rs
+++ rayna_ui/src/rayna_app.rs
@@ -44,7 +44,7 @@ impl RaynaApp {
     /// Creates a new app instance, with an [`Context`] for configuring the app
     pub fn new_ctx(_ctx: &Context) -> Self {
         info!(target: UI, "ui app init");
-        let scene = scene::stored::SIMPLE.clone();
+        let scene = scene::stored::BALLZ.clone();
         let render_opts = Default::default();
         Self {
             render_opts,

eb493a4aeea7539c654fc00fb810dd92d62e5568
v0x0g <v0x0g_dev@proton.me>
2023-12-31 15:49:50 +1000

    Use `getset` in ray.rs, and add inverse ray dir field (cached)

--- rayna_engine/src/shared/ray.rs
+++ rayna_engine/src/shared/ray.rs
@@ -1,31 +1,20 @@
+use getset::CopyGetters;
 use rayna_shared::def::types::{Number, Point3, Vector3};
 
-#[derive(Copy, Clone, PartialEq, Debug)]
+#[derive(Copy, Clone, PartialEq, Debug, CopyGetters)]
+#[getset(get_copy = "pub")]
 pub struct Ray {
     pos: Point3,
     dir: Vector3,
+    inv_dir: Vector3,
 }
 
 impl Ray {
-    /// World-space coordinate of the ray
-    #[inline(always)]
-    pub fn pos(&self) -> Point3 {
-        self.pos
-    }
-
-    /// Direction vector of the ray.
-    ///
-    /// # Requirements
-    ///     Must be normalised
-    #[inline(always)]
-    pub fn dir(&self) -> Vector3 {
-        self.dir
-    }
-
     pub fn new(pos: Point3, dir: Vector3) -> Self {
         Self {
             pos,
             dir: dir.normalize(),
+            inv_dir: dir.recip(),
         }
     }
 
@@ -35,7 +24,11 @@ impl Ray {
     /// Unsafe as it does not normalise the direction, assuming the caller
     /// provided a correct vector, possibly breaking the invariant of a normalised direction
     pub unsafe fn new_unchecked(pos: Point3, dir: Vector3) -> Self {
-        Self { pos, dir }
+        Self {
+            pos,
+            dir,
+            inv_dir: dir.recip(),
+        }
     }
 
     /// Gets the position at a given distance along the ray

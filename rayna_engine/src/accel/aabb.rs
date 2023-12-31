af84fce9a1c5915ae052166e2da5acbd7216f140
v0x0g <v0x0g_dev@proton.me>
2023-12-31 22:25:37 +1000

    Why is the code so fucking slow

--- rayna_engine/src/accel/aabb.rs
+++ rayna_engine/src/accel/aabb.rs
@@ -1,9 +1,8 @@
 use std::borrow::Borrow;
-use std::ops::Mul;
 
 use getset::*;
 
-use rayna_shared::def::types::{Number, Point3, Point4, Vector3, Vector4};
+use rayna_shared::def::types::{Number, Point3, Vector3};
 
 use crate::shared::bounds::Bounds;
 use crate::shared::ray::Ray;
@@ -75,138 +74,39 @@ impl Aabb {
 impl Aabb {
     /// Checks whether the given ray intersects with the AABB at any point within the given distance bounds
     pub fn hit(&self, ray: &Ray, bounds: &Bounds<Number>) -> bool {
-        const MODE: usize = 5;
-
-        // METHOD 1: Pete Shirley's code
-        if MODE == 1 {
-            let ro = ray.pos().to_array();
-            let ird = ray.inv_dir().to_array();
-            let min = self.min.to_array();
-            let max = self.max.to_array();
-
-            for i in 0..3_usize {
-                let (ro_i, inv_d, min_i, max_i) = (ro[i], ird[i], min[i], max[i]);
-                let mut t0 = (min_i - ro_i) * inv_d;
-                let mut t1 = (max_i - ro_i) * inv_d;
-                if inv_d.is_sign_negative() {
-                    std::mem::swap(&mut t0, &mut t1);
-                }
-
-                // The range in which the ray is 'inside' the AABB
-                // Is not within the valid range for the ray,
-                // so there is no valid intersection
-                if !bounds.range_overlaps(&t0, &t1) {
-                    return false;
-                }
-            }
-            return true;
-        }
-        // METHOD 2: Tavianator
+        // Credits: Tavianator
         // https://tavianator.com/cgit/dimension.git/tree/libdimension/bvh/bvh.c#n196
         // https://tavianator.com/2011/ray_box.html
-        else if MODE == 2 {
-            // This is actually correct, even though it appears not to handle edge cases
-            // (ray.n.{x,y,z} == 0).  It works because the infinities that result from
-            // dividing by zero will still behave correctly in the comparisons.  Rays
-            // which are parallel to an axis and outside the box will have tmin == inf
-            // or tmax == -inf, while rays inside the box will have tmin and tmax
-            // unchanged.
 
-            let tx1 = (self.min.x - ray.pos().x) * ray.inv_dir().x;
-            let tx2 = (self.max.x - ray.pos().x) * ray.inv_dir().x;
+        // This is actually correct, even though it appears not to handle edge cases
+        // (ray.n.{x,y,z} == 0).  It works because the infinities that result from
+        // dividing by zero will still behave correctly in the comparisons.  Rays
+        // which are parallel to an axis and outside the box will have tmin == inf
+        // or tmax == -inf, while rays inside the box will have tmin and tmax
+        // unchanged.
 
-            let mut tmin = Number::min(tx1, tx2);
-            let mut tmax = Number::max(tx1, tx2);
+        let tx1 = (self.min.x - ray.pos().x) * ray.inv_dir().x;
+        let tx2 = (self.max.x - ray.pos().x) * ray.inv_dir().x;
 
-            let ty1 = (self.min.y - ray.pos().y) * ray.inv_dir().y;
-            let ty2 = (self.max.y - ray.pos().y) * ray.inv_dir().y;
+        let mut tmin = Number::min(tx1, tx2);
+        let mut tmax = Number::max(tx1, tx2);
 
-            tmin = Number::max(tmin, Number::min(ty1, ty2));
-            tmax = Number::min(tmax, Number::max(ty1, ty2));
+        let ty1 = (self.min.y - ray.pos().y) * ray.inv_dir().y;
+        let ty2 = (self.max.y - ray.pos().y) * ray.inv_dir().y;
 
-            let tz1 = (self.min.z - ray.pos().z) * ray.inv_dir().z;
-            let tz2 = (self.max.z - ray.pos().z) * ray.inv_dir().z;
+        tmin = Number::max(tmin, Number::min(ty1, ty2));
+        tmax = Number::min(tmax, Number::max(ty1, ty2));
 
-            tmin = Number::max(tmin, Number::min(tz1, tz2));
-            tmax = Number::min(tmax, Number::max(tz1, tz2));
+        let tz1 = (self.min.z - ray.pos().z) * ray.inv_dir().z;
+        let tz2 = (self.max.z - ray.pos().z) * ray.inv_dir().z;
 
-            // return tmax >= Number::max(0.0, tmin) && tmin < t;
-            // We have: 0.0 => bounds.start, t => bounds.end
-            return bounds.range_overlaps(&tmin, &tmax);
-            // return (max(bounds.start, tmin) <= tmax) && (tmin < bounds.end)
-        }
-        // METHOD 3: Tavianator (Vectors)
-        // https://gist.github.com/dubik/4412640#file-bbox-cpp-L14
-        else if MODE == 3 {
-            let t1 = Vector3::mul(self.min - ray.pos(), ray.inv_dir());
-            let t2 = Vector3::mul(self.max - ray.pos(), ray.inv_dir());
+        tmin = Number::max(tmin, Number::min(tz1, tz2));
+        tmax = Number::min(tmax, Number::max(tz1, tz2));
 
-            let tmin_v = Vector3::min(t1, t2);
-            let tmax1 = Vector3::max(t1, t2);
-
-            let tmin = Vector3::min_element(tmin_v);
-            let tmax = Vector3::max_element(tmax1);
-
-            return bounds.range_overlaps(&tmin, &tmax);
-        }
-        // METHOD 4: Tavianator (Vectors + Safe SIMD)
-        // https://gist.github.com/dubik/4412640#file-bbox-cpp-L14
-        else if MODE == 4 {
-            use std::simd::prelude::*;
-            type SimdVec = Simd<Number, 4>;
-
-            let min = SimdVec::from(self.min.extend(0.).to_array());
-            let max = SimdVec::from(self.max.extend(0.).to_array());
-            let ray_pos = SimdVec::from(ray.pos().extend(0.).to_array());
-            let inv_dir = SimdVec::from(ray.inv_dir().extend(0.).to_array());
-
-            let t1_v = SimdVec::mul(min - ray_pos, inv_dir);
-            let t2_v = SimdVec::mul(max - ray_pos, inv_dir);
-
-            let tmin_v = SimdVec::simd_min(t1_v, t2_v);
-            let tmax_v = SimdVec::simd_max(t1_v, t2_v);
-
-            let tmin = SimdVec::reduce_min(tmin_v);
-            let tmax = SimdVec::reduce_max(tmax_v);
-
-            return bounds.range_overlaps(&tmin, &tmax);
-        }
-        // METHOD 5: Tavianator (Vectors + Unsafe SIMD)
-        else if MODE == 5 {
-            use std::mem::transmute;
-            use std::simd::prelude::*;
-            use std::simd::*;
-
-            unsafe {
-                type SVec = Simd<Number, 4>;
-
-                #[repr(align(32))]
-                struct S([Number; 4]);
-
-                let min = S(self.min.extend(0.).to_array());
-                let max = S(self.max.extend(0.).to_array());
-                let ray_pos = S(ray.pos().extend(0.).to_array());
-                let inv_dir = S(ray.inv_dir().extend(0.).to_array());
-
-                let min: SVec = transmute(min);
-                let max: SVec = transmute(max);
-                let ray_pos: SVec = transmute(ray_pos);
-                let inv_dir: SVec = transmute(inv_dir);
-
-                let t1_v = SVec::mul(min - ray_pos, inv_dir);
-                let t2_v = SVec::mul(max - ray_pos, inv_dir);
-
-                let tmin_v = SVec::simd_min(t1_v, t2_v);
-                let tmax_v = SVec::simd_max(t1_v, t2_v);
-
-                let tmin = SVec::reduce_min(tmin_v);
-                let tmax = SVec::reduce_max(tmax_v);
-
-                return bounds.range_overlaps(&tmin, &tmax);
-            }
-        }
-
-        unreachable!()
+        // return tmax >= Number::max(0.0, tmin) && tmin < t;
+        // We have: 0.0 => bounds.start, t => bounds.end
+        return bounds.range_overlaps(&tmin, &tmax);
+        // return (max(bounds.start, tmin) <= tmax) && (tmin < bounds.end)
     }
 }
 // endregion Impl

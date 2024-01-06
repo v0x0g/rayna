492753a03a80fd58759e6f17fa7f414841bf2b1b
v0x0g <v0x0g_dev@proton.me>
2024-01-06 13:26:13 +1000

    Implement Ord for Intersection

--- rayna_engine/src/shared/intersect.rs
+++ rayna_engine/src/shared/intersect.rs
@@ -1,6 +1,7 @@
 use crate::material::MaterialType;
 use derivative::Derivative;
 use rayna_shared::def::types::{Number, Point3, Vector3};
+use std::cmp::Ordering;
 
 /// A struct representing a ray-object intersection
 #[derive(Clone, Debug, Derivative)]
@@ -13,17 +14,36 @@ pub struct Intersection {
     /// incident ray
     ///
     /// # Invariants
-    ///     Must be normalised
+    ///  - Must be normalised
+    ///  - Cannot be zero/nan
     pub normal: Vector3,
     /// Surface normal at intersection.
     /// This should point in the *opposite* direction to the incident ray
     ///
     /// # Invariants
-    ///     Must be normalised
+    /// - Must be normalised
+    /// - Cannot be Zero/Nan
     pub ray_normal: Vector3,
     pub front_face: bool,
     /// Distance along the ray that the intersection occurred
+    ///
+    ///
     pub dist: Number,
-    #[derivative(PartialEq = "ignore")]
+    #[derivative(PartialEq = "ignore", PartialOrd = "ignore")]
     pub material: MaterialType,
+}
+
+impl Eq for Intersection {}
+
+impl PartialOrd<Self> for Intersection {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Number::partial_cmp(&self.dist, &other.dist)
+    }
+}
+
+impl Ord for Intersection {
+    fn cmp(&self, other: &Self) -> Ordering {
+        Number::partial_cmp(&self.dist, &other.dist)
+            .expect("couldn't compare intersections distances: invariant `.dist != NaN` failed")
+    }
 }

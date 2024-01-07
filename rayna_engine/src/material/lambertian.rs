ab3b88934eedb48e67603c394c9d9d258f4ebcd7
v0x0g <v0x0g_dev@proton.me>
2024-01-07 13:22:27 +1000

    Got the SIMD working.
    - `_mm_mask_loadu_ps` (or any SIMD func with mask) only available on AVX512F, which my CPU don't have :(

--- rayna_engine/src/material/lambertian.rs
+++ rayna_engine/src/material/lambertian.rs
@@ -41,18 +41,19 @@ impl Material for LambertianMaterial {
         let a = self.albedo.value(intersect, rng).0;
         let e = self.emissive.value(intersect, rng).0;
 
-        // unsafe {
-        //     use core::arch::x86_64::*;
-        //     use image::Pixel as _;
-        //     const MASK: __mmask8 = 0b1110;
-        //     let f = _mm_maskz_loadu_ps(MASK, f.as_ptr());
-        //     let a = _mm_maskz_loadu_ps(MASK, a.as_ptr());
-        //     let e = _mm_maskz_loadu_ps(MASK, e.as_ptr());
-        //     let o = _mm_fmadd_ps(f, a, e);
-        //     let mut p = [0.; 4];
-        //     _mm_mask_storeu_ps(p.as_mut_ptr(), MASK, o);
-        //     *Pixel::from_slice(&p)
-        // };
+        unsafe {
+            use core::arch::x86_64::*;
+            use image::Pixel as _;
+            // SAFETY: Loading len-3 slices into len-4 vector but should be safe since it's all on stack
+            // 4th value is discarded
+            let f = _mm_loadu_ps(f.as_ptr());
+            let a = _mm_loadu_ps(a.as_ptr());
+            let e = _mm_loadu_ps(e.as_ptr());
+            let o = _mm_fmadd_ps(f, a, e);
+            let mut p = [0.; 4];
+            _mm_storeu_ps(p.as_mut_ptr(), o);
+            *Pixel::from_slice(&p[0..3])
+        }
 
         Pixel::from([
             Channel::mul_add(f[0], a[0], e[0]),

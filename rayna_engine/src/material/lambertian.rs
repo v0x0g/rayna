9c70e9049c2a22a2d4045a983eb7885082557309
v0x0g <v0x0g_dev@proton.me>
2024-01-07 13:22:42 +1000

    Remove simd :(

--- rayna_engine/src/material/lambertian.rs
+++ rayna_engine/src/material/lambertian.rs
@@ -41,20 +41,6 @@ impl Material for LambertianMaterial {
         let a = self.albedo.value(intersect, rng).0;
         let e = self.emissive.value(intersect, rng).0;
 
-        unsafe {
-            use core::arch::x86_64::*;
-            use image::Pixel as _;
-            // SAFETY: Loading len-3 slices into len-4 vector but should be safe since it's all on stack
-            // 4th value is discarded
-            let f = _mm_loadu_ps(f.as_ptr());
-            let a = _mm_loadu_ps(a.as_ptr());
-            let e = _mm_loadu_ps(e.as_ptr());
-            let o = _mm_fmadd_ps(f, a, e);
-            let mut p = [0.; 4];
-            _mm_storeu_ps(p.as_mut_ptr(), o);
-            *Pixel::from_slice(&p[0..3])
-        }
-
         Pixel::from([
             Channel::mul_add(f[0], a[0], e[0]),
             Channel::mul_add(f[1], a[1], e[1]),

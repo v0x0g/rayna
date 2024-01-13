6ea484e956efd516c042a632a5f7fc94008e19e5
v0x0g <v0x0g_dev@proton.me>
2024-01-13 22:39:16 +1000

    Finally fix all the BVH issues: no clones (only moves) during creation

--- rayna_engine/src/accel/bvh.rs
+++ rayna_engine/src/accel/bvh.rs
@@ -56,7 +56,7 @@ impl<O: FullObject> Bvh<O> {
         let root_id = if objects.is_empty() {
             None
         } else {
-            Some(Self::generate_nodes_sah(objects.into_boxed_slice(), &mut arena))
+            Some(Self::generate_nodes_sah(objects, &mut arena))
         };
 
         // eprintln!("\n\n{:?}\n\n", root_id.debug_pretty_print(&tree));
@@ -97,101 +97,114 @@ impl<O: FullObject> Bvh<O> {
     ///
     /// # Panics
     /// The slice of `objects` passed in must be non-empty.
-    fn generate_nodes_sah(objects: Box<[O]>, arena: &mut Arena<BvhNode<O>>) -> NodeId {
-        // Can't pattern match on (owned) slices, or `Vec`s, so we use box and match that
-        return match objects {
-            box [] => panic!("internal invariant fail: must pass in a non-empty slice for objects"),
-            box [obj] => arena.new_node(BvhNode::Object(obj)),
-            box [a, b] => {
+    fn generate_nodes_sah(mut objects: Vec<O>, arena: &mut Arena<BvhNode<O>>) -> NodeId {
+        if 0 == objects.len() {
+            panic!("internal invariant fail: must pass in a non-empty slice for objects")
+        }
+
+        // I really hate this code, but there's not much I can do about it
+        // Can't match on a `Vec<O>`, can't move out of a `Box<[O]>` (even with `#![feature(box_patterns)]`)
+        // Can't use `if let Ok(..)` since it moves the Vec
+
+        objects = match <[O; 1]>::try_from(objects) {
+            Ok([obj]) => {
+                return arena.new_node(BvhNode::Object(obj));
+            }
+            Err(v) => v,
+        };
+
+        objects = match <[O; 2]>::try_from(objects) {
+            Ok([a, b]) => {
                 let aabb = Aabb::encompass(expect_aabb(&a), expect_aabb(&b));
 
                 let node = arena.new_node(BvhNode::Nested(aabb));
                 node.append_value(BvhNode::Object(a), arena);
                 node.append_value(BvhNode::Object(b), arena);
-                node
+                return node;
             }
-            objects => {
-                // This is a port of [my C# port of] [Pete Shirley's code]
-                // https://psgraphics.blogspot.com/2016/03/a-simple-sah-bvh-build.html
-                // https://3.bp.blogspot.com/-PMG6dWk1i60/VuG9UHjsdlI/AAAAAAAACEo/BS1qJyut7LE/s1600/Screen%2BShot%2B2016-03-10%2Bat%2B11.25.08%2BAM.png
+            Err(v) => v,
+        };
 
-                let n = objects.len();
-                let mut objects = Vec::from(objects);
-                let aabbs = objects.iter().map(|o| *expect_aabb(o)).collect_vec();
-                let main_aabb = Aabb::encompass_iter(&aabbs);
+        {
+            // This is a port of [my C# port of] [Pete Shirley's code]
+            // https://psgraphics.blogspot.com/2016/03/a-simple-sah-bvh-build.html
+            // https://3.bp.blogspot.com/-PMG6dWk1i60/VuG9UHjsdlI/AAAAAAAACEo/BS1qJyut7LE/s1600/Screen%2BShot%2B2016-03-10%2Bat%2B11.25.08%2BAM.png
 
-                // Find the longest axis to split along, and sort for that axis
-                // TODO: maybe choose the axis that gives the smallest overlap between the left & right splits?
-                //  This means why try `product_of(all 3 axes, all split positions)` and find the optimal by `left.len()^2 + right.len()^2`
+            let n = objects.len();
+            let aabbs = objects.iter().map(|o| *expect_aabb(o)).collect_vec();
+            let main_aabb = Aabb::encompass_iter(&aabbs);
 
-                // TODO: Sometimes this seems to generate a node with a single object.
-                //  It creates an (AABB->Object) node, which does double ray-aabb intersects (this is slow)
+            // Find the longest axis to split along, and sort for that axis
+            // TODO: maybe choose the axis that gives the smallest overlap between the left & right splits?
+            //  This means why try `product_of(all 3 axes, all split positions)` and find the optimal by `left.len()^2 + right.len()^2`
 
-                // Sort along longest axis
-                {
-                    let max_side = match main_aabb.size().into_iter().position_max_by(Number::total_cmp) {
-                        Some(0) => SplitAxis::X,
-                        Some(1) => SplitAxis::Y,
-                        Some(2) => SplitAxis::Z,
-                        None => unreachable!("Vector3::into_iter() cannot be empty iterator"),
-                        Some(x) => unreachable!("invalid axis {}", x),
-                    };
-                    Self::sort_along_aabb_axis(max_side, &mut objects);
+            // TODO: Sometimes this seems to generate a node with a single object.
+            //  It creates an (AABB->Object) node, which does double ray-aabb intersects (this is slow)
+
+            // Sort along longest axis
+            {
+                let max_side = match main_aabb.size().into_iter().position_max_by(Number::total_cmp) {
+                    Some(0) => SplitAxis::X,
+                    Some(1) => SplitAxis::Y,
+                    Some(2) => SplitAxis::Z,
+                    None => unreachable!("Vector3::into_iter() cannot be empty iterator"),
+                    Some(x) => unreachable!("invalid axis {}", x),
+                };
+                Self::sort_along_aabb_axis(max_side, &mut objects);
+            }
+
+            // Calculate the areas of the left/right AABBs, for each given split position
+            let (left_areas, right_areas) = {
+                let mut left_areas = Vec::new();
+                left_areas.resize(n, 0.);
+                let mut right_areas = Vec::new();
+                right_areas.resize(n, 0.);
+                //Calculate the area from the left towards right
+                let mut left_aabb = Aabb::default();
+                for (area, obj_aabb) in zip_eq(left_areas.iter_mut(), aabbs.iter()) {
+                    left_aabb = Aabb::encompass(&left_aabb, obj_aabb);
+                    *area = left_aabb.area();
                 }
 
-                // Calculate the areas of the left/right AABBs, for each given split position
-                let (left_areas, right_areas) = {
-                    let mut left_areas = Vec::new();
-                    left_areas.resize(n, 0.);
-                    let mut right_areas = Vec::new();
-                    right_areas.resize(n, 0.);
-                    //Calculate the area from the left towards right
-                    let mut left_aabb = Aabb::default();
-                    for (area, obj_aabb) in zip_eq(left_areas.iter_mut(), aabbs.iter()) {
-                        left_aabb = Aabb::encompass(&left_aabb, obj_aabb);
-                        *area = left_aabb.area();
-                    }
+                //Calculate the area from the right towards the left
+                let mut right_aabb = Aabb::default();
+                for (area, obj_aabb) in zip_eq(right_areas.iter_mut().rev(), aabbs.iter().rev()) {
+                    right_aabb = Aabb::encompass(&right_aabb, obj_aabb);
+                    *area = right_aabb.area();
+                }
+                (left_areas, right_areas)
+            };
 
-                    //Calculate the area from the right towards the left
-                    let mut right_aabb = Aabb::default();
-                    for (area, obj_aabb) in zip_eq(right_areas.iter_mut().rev(), aabbs.iter().rev()) {
-                        right_aabb = Aabb::encompass(&right_aabb, obj_aabb);
-                        *area = right_aabb.area();
-                    }
-                    (left_areas, right_areas)
-                };
+            // Find the most optimal split index, using the areas calculated above
+            let split_index = {
+                // NOTE: If doing in a for loop this would be `i: 0..n-1`, and `l=left[i], r=right[i+1]`
+                // This way we have non-overlapping left & right areas
+                let left_trimmed = left_areas.split_last().expect("left_area is empty").1;
+                let right_trimmed = right_areas.split_first().expect("right_area is empty").1;
+                let min_sa_idx = zip_eq(left_trimmed, right_trimmed)
+                    .enumerate()
+                    // calculate SA
+                    .map(|(i, (&l, &r))| (i as Number * l) + ((n - i - 1) as Number * r))
+                    .position_min_by(Number::total_cmp)
+                    .expect("area iters have >1 elem");
+                min_sa_idx
+            };
 
-                // Find the most optimal split index, using the areas calculated above
-                let split_index = {
-                    // NOTE: If doing in a for loop this would be `i: 0..n-1`, and `l=left[i], r=right[i+1]`
-                    // This way we have non-overlapping left & right areas
-                    let left_trimmed = left_areas.split_last().expect("left_area is empty").1;
-                    let right_trimmed = right_areas.split_first().expect("right_area is empty").1;
-                    let min_sa_idx = zip_eq(left_trimmed, right_trimmed)
-                        .enumerate()
-                        // calculate SA
-                        .map(|(i, (&l, &r))| (i as Number * l) + ((n - i - 1) as Number * r))
-                        .position_min_by(Number::total_cmp)
-                        .expect("area iters have >1 elem");
-                    min_sa_idx
-                };
+            // Split the vector into the two halves. Annoyingly there is no nice API for boxed slices or vectors
+            let (left_split, right_split) = {
+                let mut l = Vec::from(objects);
+                let r = l.split_off(split_index + 1);
+                (l, r)
+            };
 
-                // Split the vector into the two halves. Annoyingly there is no nice API for boxed slices or vectors
-                let (left_split, right_split) = {
-                    let mut l = Vec::from(objects);
-                    let r = l.split_off(split_index + 1);
-                    (l.into_boxed_slice(), r.into_boxed_slice())
-                };
+            let left_node = Self::generate_nodes_sah(left_split, arena);
+            let right_node = Self::generate_nodes_sah(right_split, arena);
 
-                let left_node = Self::generate_nodes_sah(left_split, arena);
-                let right_node = Self::generate_nodes_sah(right_split, arena);
-
-                let node = arena.new_node(BvhNode::Nested(main_aabb));
-                node.append(left_node, arena);
-                node.append(right_node, arena);
-                node
-            }
-        };
+            let node = arena.new_node(BvhNode::Nested(main_aabb));
+            node.append(left_node, arena);
+            node.append(right_node, arena);
+            return node;
+        }
     }
 }
 

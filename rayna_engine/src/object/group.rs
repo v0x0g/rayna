ce2cf6a0dc606657d986abbe6b3534cb1ecac602
v0x0g <v0x0g_dev@proton.me>
2024-01-15 16:34:48 +1000

    TODO: GroupObject struct

---
+++ rayna_engine/src/object/group.rs
@@ -0,0 +1,19 @@
+// use getset::Getters;
+// use crate::accel::bvh::Bvh;
+// use crate::object::{Object, ObjectInstance};
+//
+// /// A group of objects that are rendered as one object
+// ///
+// /// # Notes
+// /// Since this only implements [Object], and not [crate::scene::FullObject], all the sub-objects
+// /// will share the same material (once placed inside a [crate::scene::SceneObject]
+// #[derive(Clone, Debug, Getters)]
+// #[get = "pub"]
+// pub struct GroupObject<Obj: Object + Clone = ObjectInstance> {
+//     unbounded: Vec<Obj>
+//     bounded: Bvh<>
+// }
+//
+// impl Object for GroupObject {
+//
+// }

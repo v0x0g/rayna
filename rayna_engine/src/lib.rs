136cae8d3b41fd4f7058c80bca3763c9a9732a00
v0x0g <v0x0g_dev@proton.me>
2024-01-23 15:54:20 +1000

    Writing my own colour class because the `image` crate one sucks
    Former-commit-id: 0a6d0f5f09c668c6feb483f16f21ef32ac71c0bc

--- rayna_engine/src/lib.rs
+++ rayna_engine/src/lib.rs
@@ -1,12 +1,18 @@
 #![feature(array_chunks)]
-#![feature(error_generic_member_access)]
-#![feature(const_for)]
 #![feature(array_try_map)]
 #![feature(array_methods)]
+#![feature(error_generic_member_access)]
 #![feature(box_patterns)]
 #![feature(negative_impls)]
 #![feature(iter_map_windows)]
 #![feature(iter_collect_into)]
+#![feature(maybe_uninit_uninit_array)]
+#![feature(maybe_uninit_array_assume_init)]
+#![feature(const_for)]
+#![feature(const_trait_impl)]
+#![feature(const_mut_refs)]
+#![feature(const_maybe_uninit_uninit_array)]
+#![feature(const_maybe_uninit_array_assume_init)]
 
 pub mod material;
 pub mod mesh;
--- rayna_engine/src/shared/colour.rs
+++ rayna_engine/src/shared/colour.rs
@@ -1,9 +1,11 @@
-use auto_ops::*;
 use rayna_shared::def::types::Number;
+use std::array;
 use std::ops::{Deref, DerefMut, Index, IndexMut};
 
 #[derive(Copy, Clone, Debug, PartialOrd, PartialEq)]
-pub struct Colour<const N: usize = 3>([Number; N]);
+pub struct Colour<const N: usize>([Number; N]);
+
+pub type ColourRgb = Colour<3>;
 
 impl<const N: usize> Colour<N> {
     /// How many channels there are, for this colour.
@@ -14,29 +16,31 @@ impl<const N: usize> Colour<N> {
 // region Constructors
 
 impl<const N: usize> Colour<N> {
-    pub fn new(val: impl Into<[Number; N]>) -> Self { Self(val.into()) }
+    pub const fn new(val: [Number; N]) -> Self { Self(val) }
 }
 
 // endregion Constructors
 
 // region RGB Impl
 
-impl From<(Number, Number, Number)> for Colour<3> {
-    fn from(val: (Number, Number, Number)) -> Self { Self::new(val) }
+impl const From<(Number, Number, Number)> for ColourRgb {
+    fn from(val: (Number, Number, Number)) -> Self { Self::new(val.into()) }
 }
-impl From<Colour<3>> for (Number, Number, Number) {
+impl const From<ColourRgb> for (Number, Number, Number) {
     //noinspection RsLiveness - `r,g,b` are used
-    fn from(Colour::<3> { 0: [r, g, b] }: Colour) -> Self { (r, g, b) }
+    fn from(ColourRgb { 0: [r, g, b] }: ColourRgb) -> Self { (r, g, b) }
 }
+
+//poop
 
 // endregion RGB Impl
 
 // region To/From impls
 
-impl<const N: usize> From<[Number; N]> for Colour<N> {
+impl<const N: usize> const From<[Number; N]> for Colour<N> {
     fn from(val: [Number; N]) -> Self { Self::new(val) }
 }
-impl<const N: usize> From<Colour<N>> for [Number; N] {
+impl<const N: usize> const From<Colour<N>> for [Number; N] {
     //noinspection RsLiveness - `val` is used
     fn from(Colour::<N> { 0: val }: Colour<N>) -> Self { val }
 }
@@ -45,28 +49,28 @@ impl<const N: usize> From<Colour<N>> for [Number; N] {
 
 // region Iterating/Indexing
 
-impl<const N: usize> IntoIterator for Colour<N> {
+impl<const N: usize> const IntoIterator for Colour<N> {
     type Item = Number;
-    type IntoIter = std::array::IntoIter<Number, N>;
+    type IntoIter = array::IntoIter<Number, N>;
 
     fn into_iter(self) -> Self::IntoIter { self.0.into_iter() }
 }
 
-impl<const N: usize> Deref for Colour<N> {
+impl<const N: usize> const Deref for Colour<N> {
     type Target = [Number; N];
 
     fn deref(&self) -> &Self::Target { &self.0 }
 }
-impl<const N: usize> DerefMut for Colour<N> {
+impl<const N: usize> const DerefMut for Colour<N> {
     fn deref_mut(&mut self) -> &mut Self::Target { &mut self.0 }
 }
 
-impl<const N: usize> Index<usize> for Colour<N> {
+impl<const N: usize> const Index<usize> for Colour<N> {
     type Output = Number;
 
     fn index(&self, index: usize) -> &Self::Output { &self.0[index] }
 }
-impl<const N: usize> IndexMut<usize> for Colour<N> {
+impl<const N: usize> const IndexMut<usize> for Colour<N> {
     fn index_mut(&mut self, index: usize) -> &mut Self::Output { &mut self.0[index] }
 }
 
@@ -74,18 +78,40 @@ impl<const N: usize> IndexMut<usize> for Colour<N> {
 
 // region Operators
 
-impl Colour {
-    pub fn map(&self, op: impl FnMut(Number) -> Number) -> Self { self.0.map(op).into() }
+impl<const N: usize> Colour<N> {
+    #[inline]
+    pub fn map(&self, op: impl Fn(Number) -> Number) -> Self { self.0.map(op).into() }
+    #[inline]
     pub fn map2(&self, other: &Self, mut op: impl FnMut(Number, Number) -> Number) -> Self {
-        // SAFETY: Both colours are constant arrays, so this is fine to `.unwrap()`
-        std::array::from_fn(|i| op(self[i], other[i])).into()
+        array::from_fn(|i| op(self[i], other[i])).into()
     }
 }
 
-// TODO: Make this use the type parameter `N` on `Colour`
-#[rustfmt::skip] impl_op_ex!(+ |a: &Colour, b: &Colour| -> Colour { Colour::map2(a, b, Number::add) });
-#[rustfmt::skip] impl_op_ex!(- |a: &Colour, b: &Colour| -> Colour { Colour::map2(a, b, Number::sub) });
-#[rustfmt::skip] impl_op_ex!(* |a: &Colour, b: &Colour| -> Colour { Colour::map2(a, b, Number::mul) });
-#[rustfmt::skip] impl_op_ex!(/ |a: &Colour, b: &Colour| -> Colour { Colour::map2(a, b, Number::div) });
+/// Helper macro to provide implementations of operator traits
+///
+/// The functions should take in an owned `Self`-type reference
+///
+/// I would use the [auto_ops]/[impl_ops] crates, but they don't support const generics, so roll my own
+macro_rules! implement_operator {
+    (($($operator:tt)*), fn $fn_name:ident ($a:ident, $b:ident) $body:block) => {
+        implement_operator!(@inner ($($operator)*), fn $fn_name ($a:  Colour<N>, $b:  Colour<N>) -> Colour<N> $body);
+        implement_operator!(@inner ($($operator)*), fn $fn_name ($a:  Colour<N>, $b: &Colour<N>) -> Colour<N> $body);
+        implement_operator!(@inner ($($operator)*), fn $fn_name ($a: &Colour<N>, $b:  Colour<N>) -> Colour<N> $body);
+        implement_operator!(@inner ($($operator)*), fn $fn_name ($a: &Colour<N>, $b: &Colour<N>) -> Colour<N> $body);
+    };
+
+    (@inner ($($operator:tt)*), fn $fn_name:ident ($a:ident: $lhs:ty, $b:ident : $rhs:ty) -> $out:ty $body:block) => {
+        impl<const N: usize> $($operator)*<$rhs> for $lhs {
+            type Output = $out;
+
+            fn $fn_name(self, rhs: $rhs) -> Self::Output {
+                let ($a, $b) = (self.clone(), rhs.clone());
+                $body
+            }
+        }
+    };
+}
+
+implement_operator!((std::ops::Add), fn add(a, b) { Colour::map2(&a, &b, Number::add) });
 
 // endregion

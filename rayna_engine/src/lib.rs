804b9c9cbe49fc12c327202ab66f8a1f6a348c25
v0x0g <v0x0g_dev@proton.me>
2024-01-29 17:28:18 +1100

    Add ndarray crate
    Former-commit-id: f7b70aa8e6a704dd8b1d905b127ae19a3e61b775

--- rayna_engine/src/lib.rs
+++ rayna_engine/src/lib.rs
@@ -1,16 +1,17 @@
 #![feature(array_chunks)]
 #![feature(array_try_map)]
 #![feature(array_methods)]
-#![feature(error_generic_member_access)]
-#![feature(box_patterns)]
-#![feature(negative_impls)]
 #![feature(iter_map_windows)]
 #![feature(iter_collect_into)]
 #![feature(const_for)]
+#![feature(vec_into_raw_parts)]
+#![feature(negative_impls)]
+#![feature(trait_alias)]
+#![feature(new_uninit)]
+#![feature(box_patterns)]
 #![feature(const_trait_impl)]
 #![feature(const_mut_refs)]
-#![feature(new_uninit)]
-#![feature(vec_into_raw_parts)]
+#![feature(error_generic_member_access)]
 
 pub mod core;
 pub mod material;

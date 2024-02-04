9f85ec832b20c5329cd78709c9d9a1fe40f11f94
v0x0g <v0x0g_dev@proton.me>
2024-02-04 14:13:37 +1100

    Some docs for raymarched mesh
    Former-commit-id: a0fdc9962d7c1712e3bb701c3c4a82ba11c2b39c

--- rayna_engine/src/mesh/advanced/raymarched.rs
+++ rayna_engine/src/mesh/advanced/raymarched.rs
@@ -14,7 +14,11 @@ use rand_core::RngCore;
 #[derivative(Debug)]
 pub struct RaymarchedMesh {
     #[derivative(Debug = "ignore")]
+    #[get = "pub"]
     sdf: Box<dyn SdfGeneratorFunction>,
+
+    max_iterations: usize,
+    epsilon: Number,
 }
 
 pub trait SdfGeneratorFunction: Fn(Point3) -> Number + Send + Sync + DynClone {}
@@ -24,14 +28,36 @@ dyn_clone::clone_trait_object!(SdfGeneratorFunction);
 // region Constructors
 
 impl RaymarchedMesh {
+    pub const DEFAULT_EPSILON: Number = 1e-7;
+    pub const DEFAULT_ITERATIONS: usize = 150;
+
     /// Creates a new mesh from the given isosurface, as defined by the **Signed-Distance Function** (**SDF**)
     ///
     /// # Arguments
     ///
-    /// * `resolution`: How dense the resulting mesh should be.
-    /// The resulting mesh has dimensions of a `N*N*N` grid, where `N = resolution`
     /// * `sdf`: The **SDF** that defines the surface for the mesh
-    pub fn new<F: SdfGeneratorFunction + 'static>(sdf: F) -> Self { Self { sdf: Box::new(sdf) } }
+    pub fn new<F: SdfGeneratorFunction + 'static>(sdf: F) -> Self {
+        Self {
+            sdf: Box::new(sdf),
+            epsilon: Self::DEFAULT_EPSILON,
+            max_iterations: Self::DEFAULT_ITERATIONS,
+        }
+    }
+
+    /// Creates a new mesh from the given isosurface, as defined by the **Signed-Distance Function** (**SDF**)
+    ///
+    /// # Arguments
+    ///
+    /// * `sdf`: The **SDF** that defines the surface for the mesh
+    /// * `max_iterations`: The maximum number of ray-marching steps allowed for intersections
+    /// * `epsilon`: The distance threshold at which a ray is considered to have intersected with the surface
+    pub fn new_custom<F: SdfGeneratorFunction + 'static>(sdf: F, max_iterations: usize, epsilon: Number) -> Self {
+        Self {
+            sdf: Box::new(sdf),
+            epsilon,
+            max_iterations,
+        }
+    }
 }
 
 // endregion Constructors
@@ -48,8 +74,7 @@ impl MeshProperties for RaymarchedMesh {
 
 impl Mesh for RaymarchedMesh {
     fn intersect(&self, ray: &Ray, interval: &Interval<Number>, _rng: &mut dyn RngCore) -> Option<Intersection> {
-        const MAX_ITERATIONS: usize = 150;
-        const EPSILON: Number = 1e-7;
+        let epsilon = self.epsilon;
 
         // Start point at earliest pos on ray, or ray origin if unbounded
         let mut total_dist = interval.start.unwrap_or(0.0);
@@ -65,15 +90,15 @@ impl Mesh for RaymarchedMesh {
 
             // Arbitrarily close to surface, counts as an intersection
             // Also needs to be in valid bounds
-            if dist.abs() < EPSILON && interval.contains(&total_dist) {
+            if dist.abs() < epsilon && interval.contains(&total_dist) {
                 // let point_pos = point + Vector3::splat(EPSILON);
                 // let point_neg = point - Vector3::splat(EPSILON);
                 let p = point;
                 let normal = Vector3::normalize(
                     [
-                        (self.sdf)((p.x + EPSILON, p.y, p.z).into()) - (self.sdf)((p.x - EPSILON, p.y, p.z).into()),
-                        (self.sdf)((p.x, p.y + EPSILON, p.z).into()) - (self.sdf)((p.x, p.y - EPSILON, p.z).into()),
-                        (self.sdf)((p.x, p.y, p.z + EPSILON).into()) - (self.sdf)((p.x, p.y, p.z - EPSILON).into()),
+                        (self.sdf)((p.x + epsilon, p.y, p.z).into()) - (self.sdf)((p.x - epsilon, p.y, p.z).into()),
+                        (self.sdf)((p.x, p.y + epsilon, p.z).into()) - (self.sdf)((p.x, p.y - epsilon, p.z).into()),
+                        (self.sdf)((p.x, p.y, p.z + epsilon).into()) - (self.sdf)((p.x, p.y, p.z - epsilon).into()),
                     ]
                     .into(),
                 );
@@ -91,7 +116,7 @@ impl Mesh for RaymarchedMesh {
             }
 
             // Exceeded the limit
-            if i > MAX_ITERATIONS {
+            if i > self.max_iterations {
                 return None;
             }
 

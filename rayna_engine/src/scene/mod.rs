18750311d97813e8c33dcf7d5ef2a38b074c13d9
v0x0g <v0x0g_dev@proton.me>
2024-06-23 21:16:13 +1000

    Remove Object, Skybox bounds from Scene

--- rayna_engine/src/scene/mod.rs
+++ rayna_engine/src/scene/mod.rs
@@ -1,19 +1,15 @@
 use crate::material::MaterialInstance;
 use crate::mesh::MeshInstance;
-use crate::object::{Object, ObjectInstance};
+use crate::object::ObjectInstance;
 use crate::scene::camera::Camera;
-use crate::skybox::{Skybox, SkyboxInstance};
+use crate::skybox::SkyboxInstance;
 use crate::texture::TextureInstance;
 
 pub mod camera;
 pub mod stored;
 
 #[derive(Clone, Debug)]
-pub struct Scene<Obj = ObjectInstance<MeshInstance, MaterialInstance<TextureInstance>>, Sky = SkyboxInstance>
-where
-    Obj: Object + Clone,
-    Sky: Skybox + Clone,
-{
+pub struct Scene<Obj = ObjectInstance<MeshInstance, MaterialInstance<TextureInstance>>, Sky = SkyboxInstance> {
     pub objects: Obj,
     pub skybox: Sky,
     pub camera: Camera,

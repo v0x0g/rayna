332ea715275589d2c3b29960167a1d01cf3c0d32
v0x0g <v0x0g_dev@proton.me>
2024-06-25 11:19:47 +1000

    Update docs on `validate` module

--- rayna_engine/src/shared/validate.rs
+++ rayna_engine/src/shared/validate.rs
@@ -1,10 +1,15 @@
-use crate::core::types::{Colour, Number, Point2, Point3, Vector2, Vector3};
+use crate::core::types::{Channel, Colour, Number, Point2, Point3, Vector2, Vector3};
 use crate::shared::intersect::Intersection;
 use crate::shared::interval::Interval;
 use crate::shared::ray::Ray;
 use approx::*;
 use std::borrow::Borrow;
 
+/// Macro that inserts a `return` statement if debug assertions are disabled
+///
+/// Required because we use some of the asserts from [`approx`],
+/// which don't have a [`debug_assert!`] equivalent, so the only way to not
+/// execute them in release is to return.
 macro_rules! debug_assert_only {
     () => {
         if cfg!(not(debug_assertions)) {
@@ -17,20 +22,24 @@ pub const EPSILON: Number = 1e-6;
 pub const ULPS: usize = 4;
 pub const RELATIVE: Number = 1e-3;
 
+/// Check is not [`Number::NAN`] and not [`Number::INFINITY`]
 #[inline(always)]
 #[track_caller]
 pub fn number(val: impl Borrow<Number>) {
     debug_assert_only!();
-
     let val = val.borrow();
+
     assert!(!val.is_nan(), "should not be nan; val: {val}");
+    assert!(!val.is_infinite(), "should not be inf; val: {val}");
 }
 
+/// Check is a valid vector, and normalised
 #[inline(always)]
 #[track_caller]
 pub fn normal3(n: impl Borrow<Vector3>) {
     debug_assert_only!();
     let n = n.borrow();
+
     vector3(n);
     assert!(
         n.is_normalized(),
@@ -38,11 +47,14 @@ pub fn normal3(n: impl Borrow<Vector3>) {
         n.length()
     );
 }
+
+/// Check is a valid vector, and normalised
 #[inline(always)]
 #[track_caller]
 pub fn normal2(n: impl Borrow<Vector2>) {
     debug_assert_only!();
     let n = n.borrow();
+
     vector2(n);
     assert!(
         n.is_normalized(),
@@ -51,55 +63,86 @@ pub fn normal2(n: impl Borrow<Vector2>) {
     );
 }
 
+/// Check all components are valid numbers
 #[inline(always)]
 #[track_caller]
 pub fn point3(v: impl Borrow<Point3>) {
     debug_assert_only!();
-    let v = v.borrow();
-    assert!(!v.is_nan(), "should not be nan; vec: {v:?}");
+    let p = v.borrow();
+
+    for c in p.as_array() {
+        number(c);
+    }
 }
 
+/// Check all components are valid numbers
 #[inline(always)]
 #[track_caller]
 pub fn vector2(v: impl Borrow<Vector2>) {
     debug_assert_only!();
     let v = v.borrow();
-    assert!(!v.is_nan(), "should not be nan; vec: {v:?}");
+
+    for c in v.as_array() {
+        number(c);
+    }
 }
 
+/// Check all components are valid numbers
 #[inline(always)]
 #[track_caller]
 pub fn vector3(v: impl Borrow<Vector3>) {
     debug_assert_only!();
     let v = v.borrow();
-    assert!(!v.is_nan(), "should not be nan; vec: {v:?}");
+
+    for c in v.as_array() {
+        number(c);
+    }
 }
 
+/// Check position and direction are valid (ignore [`Ray::inv_dir`])
 #[inline(always)]
 #[track_caller]
 pub fn ray(r: impl Borrow<Ray>) {
     debug_assert_only!();
     let r = r.borrow();
+
     normal3(r.dir());
+    point3(r.pos());
 }
 
+/// Same checks as [`self::number`], as well as non-negative
 #[inline(always)]
 #[track_caller]
-pub fn colour(c: impl Borrow<Colour>) {
+pub fn channel(c: impl Borrow<Channel>) {
     debug_assert_only!();
     let c = c.borrow();
-    assert!(
-        c.0.iter().all(|&chan| chan >= 0.0),
-        "channels should be >= 0; col: {c:?}"
-    )
+
+    assert!(!c.is_nan(), "should not be nan; val: {c}");
+    assert!(!c.is_infinite(), "should not be inf; val: {c}");
+    assert!(*c >= 0.0, "channel should be >= 0; chan: {c:?}");
 }
 
+/// Check all channels are valid
+#[inline(always)]
+#[track_caller]
+pub fn colour(col: impl Borrow<Colour>) {
+    debug_assert_only!();
+    let col = col.borrow();
+
+    for c in col.into_iter() {
+        channel(c);
+    }
+}
+
+/// Check UV coordinates are in the range `[0..1]`, and not [`Number::NAN`]
 #[inline(always)]
 #[track_caller]
 pub fn uv(uv: impl Borrow<Point2>) {
     debug_assert_only!();
     let uv = uv.borrow();
+
     assert!(!uv.is_nan(), "should not be nan; uvs: {uv:?}");
+
     assert!(
         (uv.cmpge(Point2::ZERO) & uv.cmple(Point2::ONE)).all(),
         "uv coordinates should be `0..=1`; uv: {uv:?}"
@@ -107,6 +150,9 @@ pub fn uv(uv: impl Borrow<Point2>) {
 }
 
 /// Asserts that an intersection was valid
+///
+/// Validates all sub-fields (`ray`, `uv`, etc), as well as checking that
+/// the intersection position matches the distance along the ray
 #[inline(always)]
 #[track_caller]
 pub fn intersection(
@@ -115,15 +161,15 @@ pub fn intersection(
     interval: impl Borrow<Interval<Number>>,
 ) {
     debug_assert_only!();
-
     let intersect = intersect.borrow();
     let interval = interval.borrow();
     let ray = ray.borrow();
 
     uv(&intersect.uv);
-
     point3(intersect.pos_w);
     number(intersect.dist);
+    normal3(intersect.ray_normal);
+    normal3(intersect.normal);
 
     assert!(
         interval.contains(&intersect.dist),
@@ -149,7 +195,4 @@ pub fn intersection(
         ray = ray,
         r_pos = ray.at(intersect.dist)
     );
-
-    normal3(intersect.ray_normal);
-    normal3(intersect.normal);
 }
